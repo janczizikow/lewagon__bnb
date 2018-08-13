2e2cf0f09380a70899710b8e4ca20b5741a48137
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-13 15:23:36 +0200

    Add devise gem + setup user model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Devise setup
+  config.action_mailer.default_url_options = { host: 'TODO: REPLACE WITH HOSTNAME' }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
