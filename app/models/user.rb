2c03c868e2c10f01fa0d9a371151938372941579
Hien Nguyen <mhien.ng@gmail.com>
2018-08-13 18:34:01 +0200

    Updated validation for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,13 @@ class User < ApplicationRecord
   has_many :owned_boats, dependent: :destroy, class_name: 'Boat'
   has_many :bookings
   has_many :rented_boats, through: :bookings, source: :boat
+
+  # Validates user name and email
   validates :first_name, :last_name, presence: true
-  validates :email, confirmation: true, uniqueness: { case_sensitive: false }
-  validates :email_confirmation, presence: true
-  validates :password, presence: true, length: { in: 6..20 }
+  validates :email, uniqueness: { case_sensitive: false }
+
+  # Validate password
+  validates_length_of :password, in: 6..20, on: :create
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
