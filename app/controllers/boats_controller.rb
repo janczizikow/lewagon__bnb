96605657e09eb580ff43899896e1072cb3cbe08b
malcolmtorino <malcolmbyu@gmail.com>
2018-08-14 14:02:54 +0200

    Creating BoatsController + routes

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,50 @@
+class BoatsController < ApplicationController
+  before_action :set_boat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @boat
+  end
+
+  def update
+    if @boat.update(boat_params)
+      redirect_to boat_path(@boat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @boat.destroy
+  end
+
+  private
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+  end
+
+  def boat_params
+    params[:boat].permit(:title, :price, :description, :city, :capacity, :is_available, :has_captain, :license_plate)
+  end
+end
