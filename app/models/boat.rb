9c00bdc401013be06a5fad99594698ad91e08388
Jan Spoerer <jan.spoerer@whu.edu>
2018-08-14 14:16:44 +0200

    add photos to boats

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,8 +1,11 @@
 class Boat < ApplicationRecord
   # TODO: make an alias for user => should be "owner"
   # belongs_to :owner, class_name: "User"
+
   belongs_to :user
   has_many :bookings
   validates :title, :description, :city, :price, :capacity, :license_plate, presence: true
   validates :license_plate, uniqueness: true
+
+  mount_uploader :photo, PhotoUploader
 end
