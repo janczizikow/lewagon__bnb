971360336ffa0fdbcf95c6e1e68b578b9fb72bab
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-16 15:05:01 +0200

    Geocoding & Code splitting

---
+++ app/javascript/components/map.js
@@ -0,0 +1,32 @@
+import GMaps from "gmaps/gmaps.js";
+import styles from './mapStyles';
+
+const addMap = () => {
+  const mapElement = document.getElementById("map");
+
+  if (mapElement) {
+    mapElement.style.height = `${window.innerHeight - 92}px`;
+
+    // FIXME: HARDCODED VALUES FOR MAP POSITION
+    const map = new GMaps({ el: "#map", lat: 52.493157, lng: 13.452119 });
+
+    map.addStyle({styles, mapTypeId: "map_style"});
+    map.setStyle("map_style");
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+
+    if (markers) {
+      map.addMarkers(markers);
+      if (markers.length === 0) {
+        map.setZoom(2);
+      } else if (markers.length === 1) {
+        map.setCenter(markers[0].lat, markers[0].lng);
+        map.setZoom(14);
+      } else {
+        map.fitLatLngBounds(markers);
+      }
+    }
+  }
+};
+
+export { addMap };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,11 @@
 /* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import "bootstrap";
+import "lazysizes/lazysizes";
+import "lazysizes/plugins/blur-up/ls.blur-up";
 import { showHideNavbar } from '../components/navbar';
 
 document.addEventListener("DOMContentLoaded", () => {
   showHideNavbar();
 });
-
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
-ReactRailsUJS.useContext(componentRequireContext)
